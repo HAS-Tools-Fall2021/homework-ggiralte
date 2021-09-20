2a6687779cf3bcee91acca7d7e8bdb602931bd23
Gigi Giralte <ggiralte@email.arizona.edu>
2021-09-20 10:37:45 -0700

    Forecast Sub Week 4

---
+++ Forecast_Submissions/assignment_4_sub/giralte_HW4.md
@@ -0,0 +1,16 @@
+# Gigi Giralte
+**9/20/21** \
+**Assignment 4**
+
+I believe that the flow for the week of 9/19 - 9/25 will be 90 cfs based on the fact that the flow this past week was in the higher 90s. For the week of 9/26 to 10/2 I believe that the flow will be 85 cfs because the monsoon season should be over by the end of September and there shouldn't be much rain.
+
+**Question 3:** \
+My prediction for the flow for this week is 90 cfs and in the month of September, the flow was greater than 90 cfs 705 times. The flow was greater than 85 cfs 757 times.
+
+**Question 4:** \
+Since there were more active monsoon years between 1990 and 2000, the flow would be greater than 90 cfs a lot more frequently than in between 2001 and 2021. However, the data does not agree with this claim in that the cfs was greater than 90 cfs 309 times between 1990 and 2000 and 396 times between 2001 and 2021. This could potentially be due to climate change and an increase in more severe storms. However, due to the lack in the monsoon in frequent years, that's why I presumed that the flow between 1990 and 2000 would be greater.
+
+Between 1990 and 2000, the flow was greater than 85 cfs 314 times and then 443 times for 2001-2021. 
+
+**Question 5:** \
+The flow was greater than 90 cfs 368 times for first half of September and 361 times for the second half of September. For the flow of 85 cfs, in the first half of September, the flow was greater 384 times and then 398 times for the second half.
--- Forecast_Submissions/assignment_4_sub/week4_code.py
+++ Forecast_Submissions/assignment_4_sub/week4_code.py
@@ -16,10 +16,10 @@ print(os.getcwd())
 print(filepath)
 
 # %%
-# DON'T change this part -- this creates the lists you
+# DON'T change this part -- this creates the lists you 
 # should use for the rest of the assignment
 # no need to worry about how this is being done now we will cover
-# this in later sections.
+# this in later sections. 
 
 #Read the data into a pandas dataframe
 data=pd.read_table(filepath, sep = '\t', skiprows=30,
@@ -40,7 +40,7 @@ del(data)
 
 # %%
 # Starter Code:
-# Start making your changes here.
+# Start making your changes here. 
 
 #NOTE: You will be working with the numpy array 'flow_data'
 # Flow data has a row for every day and 4 columns:
@@ -63,14 +63,14 @@ print(flow_count)
 
 # Here is the same thing broken out into multiple lines:
 flow_test = flow_data[:, 3] > 600  # Note that this returns a 1-d array that has an entry for every day of the timeseies (i.e. row) with either a true or a fals
-month_test = flow_data[:, 1] ==7   # doing the same thing but testing if month =7
+month_test = flow_data[:, 1] ==7   # doing the same thing but testing if month =7 
 combined_test = flow_test & month_test  # now looking at our last two tests and finding when they are both true
 flow_count = np.sum(combined_test) # add up all the array (note Trues = 1 and False =0) so by default this counts all the times our criteria are true
 print(flow_count)
 
 #__________________________
-## Example 2: Calculate the average flow for these same criteria
-# 2.a How to do it with one line of code:
+## Example 2: Calculate the average flow for these same criteria 
+# 2.a How to do it with one line of code: 
 # Note this is exactly like the line above exexpt now we are grabbing out the flow data
 # and then taking the averge
 flow_mean = np.mean(flow_data[(flow_data[:,3] > 600) & (flow_data[:,1]==7),3])
@@ -89,7 +89,7 @@ print('And has an average value of', np.round(flow_mean,2), "when this is true")
 # step 1: Use the linspace  funciton to create a set  of evenly spaced bins
 mybins = np.linspace(0, 1000, num=15)
 # another example using the max flow to set the upper limit for the bins
-#mybins = np.linspace(0, np.max(flow_data[:,3]), num=15)
+#mybins = np.linspace(0, np.max(flow_data[:,3]), num=15) 
 
 #Step 2: plotting the histogram
 plt.hist(flow_data[:,3], bins = mybins)
@@ -100,36 +100,38 @@ plt.ylabel('Count')
 #__________________________
 ## Example 4: Get the quantiles of flow
 
-# 4.a  Apply the np.quantile function to the flow column
+# 4.a  Apply the np.quantile function to the flow column 
 # grab out the 10th, 50th and 90th percentile flow values
 flow_quants1 = np.quantile(flow_data[:,3], q=[0,0.1, 0.5, 0.9])
 print('Method one flow quantiles:', flow_quants1)
 
-# 4.b  use the axis=0 argument to indicate that you would like the funciton
-# applied along columns. In this case you will get quantlies for every column of the
-# data automatically
+# 4.b  use the axis=0 argument to indicate that you would like the funciton 
+# applied along columns. In this case you will get quantlies for every column of the 
+# data automatically 
 flow_quants2 = np.quantile(flow_data, q=[0,0.1, 0.5, 0.9], axis=0)
 #note flow_quants2 has 4 columns just like our data so we need to say flow_quants2[:,3]
-# to extract the flow quantiles for our flow data.
-print('Method two flow quantiles:', flow_quants2[:,3])
+# to extract the flow quantiles for our flow data. 
+print('Method two flow quantiles:', flow_quants2[:,3]) 
 
-# %%
+# %% 
+# Question 1: 
 # 1. Year
 # 2. Month
 # 3. Day of the month
 # 4. Flow value in CFS
 
 flow_test = flow_data[:, 3] < 85  # Note that this returns a 1-d array that has an entry for every day of the timeseies (i.e. row) with either a true or a fals
-month_test = flow_data[:, 1] == 9   # doing the same thing but testing if month =7
+month_test = flow_data[:, 1] == 9   # doing the same thing but testing if month =7 
 year_test = flow_data[:, 0] == 2012
 combined_test = flow_test & month_test & year_test # now looking at our last two tests and finding when they are both true
 flow_count = np.sum(combined_test) # add up all the array (note Trues = 1 and False =0) so by default this counts all the times our criteria are true
 print(flow_count)
 
 # %%
-mybins = np.linspace(0, 75, num=15)
+# Question 1: 
+mybins = np.linspace(0, 90, num=15)
 # another example using the max flow to set the upper limit for the bins
-#mybins = np.linspace(0, np.max(flow_data[:,3]), num=15)
+#mybins = np.linspace(0, np.max(flow_data[:,3]), num=15) 
 
 #Step 2: plotting the histogram
 plt.hist(flow_data[:,3], bins = mybins)
@@ -137,3 +139,19 @@ plt.title('Streamflow')
 plt.xlabel('Flow [cfs]')
 plt.ylabel('Count')
 # %%
+# Question 3:
+
+flow_count = np.sum((flow_data[:,3] > 85) & (flow_data[:,1]== 9))
+print(flow_count)
+# %%
+# Question 4:
+
+flow_count_yr = np.sum((flow_data[:,3] > 85) & (flow_data[:,1] == 9) & (flow_data[:,0] >= 2001))
+print(flow_count_yr)
+# %%
+# Question 5:
+
+first_half = np.sum((flow_data[:,3] > 85) & (flow_data[:,1] == 9) & (flow_data[:,2] <= 15))
+print(first_half)
+second_half = np.sum((flow_data[:,3] > 85) & (flow_data[:,1] == 9) & (flow_data[:,2] >= 15))
+print(second_half)
