00b27565260ddde09c683d0ee497518a42632795
Gigi Giralte <ggiralte@email.arizona.edu>
2021-10-17 17:21:15 -0700

    Week 8 Assignment

---
+++ Forecast_Submissions/assignment_8_sub/giralte_HW8.md
@@ -0,0 +1,25 @@
+# Gigi Giralte
+**10/18/21** \
+**Assignment 8**
+
+### How I Generated my Forecasts:
+For my first forecast, I took the average of the past week's flow to determine what my forecasted flow would be for this week (157cfs). For my 2 week forecast, I used timeseries. I first averaged the flow in all of the days in October from 1990-2020 and then I added the standard deviations of these past three weeks together to get the forecasted flow for this week (146cfs). When I tried just doing the mean or the average of October 24-30, I got a really high value and so I decided to piece together a flow by doing the standard deviation of the past 3 weeks.
+
+### Graphs:
+Graph 1:\
+![](assets/giralte_HW8-7e13697a.png)
+
+Graph 2:\
+![](assets/giralte_HW8-250f43c2.png)
+
+Graph 3:\
+![](assets/giralte_HW8-6eef1ff9.png)
+
+### Peer Evaluation Reflection:
+I didn't really receive any good suggestions for specific parts of my code. However, I did work on trying to make it clearer with more comments to explain more of what I'm doing.
+
+### Timeseries:
+I used timeseries in the same way that we did during our practice during class where we took the mean for all of the days in October from 1990-2020.
+
+### Script I'm Proud Of:
+I really like my second plot. I'm proud of the code, specifically with the index actually, and I like how I "zoomed in" on this week's days. I also think the plot turns out really good and I enjoy how clean it looks with the location of the legend.
---
+++ Forecast_Submissions/assignment_8_sub/giralte_HW8_v2.py
@@ -0,0 +1,119 @@
+# %%
+# Import packages
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+import random
+
+# %%
+# Set the file name and path to where you have stored the data
+filename = 'streamflow_week8.txt'
+filepath = os.path.join('../../data', filename)
+print(os.getcwd())
+print(filepath)
+
+# %%
+# Read the data into a pandas dataframe
+data = pd.read_table(filepath, sep='\t', skiprows=30,
+                     names=['agency_cd', 'site_no', 'datetime', 'flow', 'code'],
+                     parse_dates=['datetime']
+                     )
+
+# Expand the dates to year month day
+data['year'] = pd.DatetimeIndex(data['datetime']).year
+data['month'] = pd.DatetimeIndex(data['datetime']).month
+data['day'] = pd.DatetimeIndex(data['datetime']).day
+data['dayofweek'] = pd.DatetimeIndex(data['datetime']).dayofweek
+
+datai = data.copy()
+datai = datai.set_index('datetime')
+
+# %%
+# Plot 1:
+# This plot shows what the flow has been for this current week (10/10-10/16)
+fig, ax = plt.subplots()
+ax.plot(datai.index.values, datai['flow'], color='aqua', label='this week')
+ax.set(title="This Week's Flow, 10/10 - 10/16", xlabel='Date', ylabel='Flow in CFS',
+       xlim=[datetime.date(2021, 10, 10), datetime.date(2021, 10, 16)],
+       ylim=[0, 175])
+ax.legend(loc='lower right')
+plt.setp(ax.get_xticklabels(), rotation=45)  # This rotates the x-axis labels so that they fit better on the plot
+plt.show
+fig.savefig("Oct10_Oct16_Flow.png")
+
+# %%
+# Plot 2:
+# This plot shows what the flow has been in the past for October 17-23
+fig, ax = plt.subplots()
+for i in range(2009, 2021):  # The for loop is necessary to go through a range of years
+        plot_data = datai[(datai['year'] == i) & (datai['month'] == 10)]
+        ax.plot(plot_data['day'], plot_data['flow'],
+                label=i)
+        ax.set(title="October Flow, 17-23", xlim=[17, 23], ylim=[50, 300])
+        ax.legend(loc='upper center', ncol=4)  # This is the location of the legend and makes it into 4 columns
+plt.show()
+fig.savefig("Oct_Trends_17_23.png")
+
+# %% 
+# Plot 3:
+# This plot shows the mean flow for each day from October 24-30 from 1990-2020
+oct = datai[datai.index.month == 10]
+datai['day'] = datai.index.day
+oct_mean = datai.groupby('day').mean()  # This gets the mean for each day 
+
+fig, ax = plt.subplots()
+ax.plot(oct_mean['flow'], color='aqua')
+ax.set(title="Oct 24-30 Flow Average", xlabel='Date', ylabel='Flow in CFS',
+        xlim=[24,30], ylim=[200,350])
+plt.show
+fig.savefig("Oct_24_40_Previous Flow.png")
+
+# %%
+# Week 1 (10/17-10/23) Forecast
+
+
+def week_1(day_start, day_end): 
+        '''
+        This function determines what my forecasted flow is for 10/17-10/23 based on the average of this past week's flow. 
+
+        Parameters:
+        "day_start" represents this past Sunday, the 10th.
+        "day_end" represents Friday, the 16th.
+
+        Output:
+        This funcion returns a print statement that provides my forecasted flow for 10/17-10/23
+        based on a random number in between the maximum and minimum flow values for this current week. 
+        '''
+        data_range = data[(data['year'] == 2021) & (data['month'] == 10) & (data['day'] >= day_start) & (data['day'] <= day_end)]
+        forecast_mean = np.mean(data_range['flow'])
+        week_1_forecast = print("The forecast for 10/10-10/16 (week 1) is:", forecast_mean, "cfs")
+        return(week_1_forecast)
+
+week_1(10, 16)
+
+# %%
+# Week 2 (10/24-10/30) Forecast
+
+
+def week_2(month): 
+        '''
+        This function determines what my forecasted flow is for week 2 based on the standard deviation of the mean flow in October over the years.
+
+        Parameters:
+        "month" represents the number of the month you want, int (October = 10)
+
+        Output:
+        This function returns a print statement that provides my forecasted flow for week 2 based
+        on what the highest average flow was from October 17-23 between 2005 and 2020. 
+        '''
+        mon = datai[datai.index.month == month]
+        datai['day'] = datai.index.day
+        oct_mean = datai.groupby('day').mean()
+        forecast = (oct_mean['flow'][17:23].std()) + (oct_mean['flow'][24:30].std()) + (oct_mean['flow'][10:16].std())
+        week_2_forecast = print("The forecast for 10/17-10/23 (week 2) is:", forecast, "cfs")
+        return(week_2_forecast)
+
+week_2(10)
+# %%
